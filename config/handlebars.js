b3f1cc888ecafcfeb4a7578e4aa2e6197288d559
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-01-29 23:03:31 +0200

    views path fix

--- config/handlebars.js
+++ config/handlebars.js
@@ -1,10 +1,16 @@
 import handlebars from "express-handlebars";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 export default function handlebarsConfig(app) {
   const hbs = handlebars.create({
     extname: "hbs",
   });
 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
   app.engine(".hbs", hbs.engine);
   app.set("view engine", ".hbs");
+  app.set("views", path.join(__dirname, "../src/views"));
 }
