8832a728c0750f8ddce667182d4058a70c7ccf5b
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-01-30 22:23:28 +0200

    Added express-validator validation for user register and login

---
+++ src/express-validator/user.js
@@ -0,0 +1,84 @@
+import { body } from "express-validator";
+import { userValidationConstants } from "../validationConstants/user.js";
+
+export const registerValidator = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage(userValidationConstants.username.requiredErrorMessage)
+    .isLength({
+      min: userValidationConstants.username.minLength,
+      max: userValidationConstants.username.maxLength,
+    })
+    .withMessage(userValidationConstants.username.lengthErrorMessage),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage(userValidationConstants.email.requiredErrorMessage)
+    .isEmail()
+    .withMessage(userValidationConstants.email.emailErrorMessage)
+    .matches(userValidationConstants.email.emailRegex)
+    .withMessage(userValidationConstants.email.emailErrorMessage),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage(userValidationConstants.password.requiredErrorMessage)
+    .isLength({
+      min: userValidationConstants.password.minLength,
+      max: userValidationConstants.password.maxLength,
+    })
+    .withMessage(userValidationConstants.password.lengthErrorMessage),
+  body("repeatPassword")
+    .trim()
+    .notEmpty()
+    .withMessage(userValidationConstants.password.requiredErrorMessage)
+    .isLength({
+      min: userValidationConstants.password.minLength,
+      max: userValidationConstants.password.maxLength,
+    })
+    .withMessage(userValidationConstants.password.lengthErrorMessage)
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords do not match !"),
+  body("imageUrl")
+    .trim()
+    .notEmpty()
+    .withMessage(userValidationConstants.imageUrl.requiredErrorMessage)
+    .isLength({
+      min: userValidationConstants.imageUrl.minLength,
+      max: userValidationConstants.imageUrl.maxLength,
+    })
+    .withMessage(userValidationConstants.imageUrl.lengthErrorMessage)
+    .matches(userValidationConstants.imageUrl.imageUrlRegex)
+    .withMessage(userValidationConstants.imageUrl.imageUrlErrorMessage),
+];
+
+export const loginValidator = [
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage(userValidationConstants.email.requiredErrorMessage)
+    .isEmail()
+    .withMessage(userValidationConstants.email.emailErrorMessage)
+    .matches(userValidationConstants.email.emailRegex)
+    .withMessage(userValidationConstants.email.emailErrorMessage),
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage(userValidationConstants.username.requiredErrorMessage)
+    .isLength({
+      min: userValidationConstants.username.minLength,
+      max: userValidationConstants.username.maxLength,
+    })
+    .withMessage(userValidationConstants.username.lengthErrorMessage),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage(userValidationConstants.password.requiredErrorMessage)
+    .isLength({
+      min: userValidationConstants.password.minLength,
+      max: userValidationConstants.password.maxLength,
+    })
+    .withMessage(userValidationConstants.password.lengthErrorMessage),
+];
