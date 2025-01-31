3f495248c3e6ab6ee07c7808bb8ceeb17ec2204a
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-01-31 16:52:22 +0200

    Fix the style href because it didn't load

--- config/express.js
+++ config/express.js
@@ -8,6 +8,12 @@ export default function expressConfig(app) {
   app.use(express.json());
   app.use(cookieParser(secret));
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static("static"));
+  app.use(express.static("static", {
+    setHeaders: (res, path) => {
+      if (path.endsWith('.css')) {
+        res.set('Content-Type', 'text/css');
+      }
+    }
+  }));
   app.use(auth());
 }
