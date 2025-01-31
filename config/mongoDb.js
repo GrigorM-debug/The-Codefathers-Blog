fc28ce5a7dad74c80207706d586feaaea1fd6b72
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-01-31 17:40:20 +0200

    Added dotenv package and .envfile. Also added email js packege

--- config/mongoDb.js
+++ config/mongoDb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export default async function mongoDbConfig() {
-  const connectionString = "mongodb://localhost:27017/TheCodeFathersDb";
+  const connectionString = process.env.MONGO_DB_CONNECTION_STRING;
 
   await mongoose
     .connect(connectionString)
--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ import expressConfig from "../config/express.js";
 import routes from "../config/routes.js";
 import handlebarsConfig from "../config/handlebars.js";
 import mongoDbConfig from "../config/mongoDb.js";
+import "dotenv/config";
 
 const app = express();
-const appPort = 3000;
+const appPort = process.env.PORT;
 
 async function start() {
   expressConfig(app);
