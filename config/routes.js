2eafc2008790cc0e8e97819529dae205f855b1df
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-01-31 16:51:40 +0200

    Added contact page

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,9 @@
 import { homeController } from "../src/controllers/home.js";
 import userRouter from "../src/controllers/user.js";
+import contactRouter from "../src/controllers/contact.js";
 
 export default function routes(app) {
   app.get("/", homeController);
+  app.use(contactRouter);
   app.use(userRouter);
 }
