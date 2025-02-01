3ceb7994feb5f5ef9821ac3f8654c57c3b169244
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-02-01 18:30:40 +0200

    Added create post page

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import auth from "../src/middlewares/auth.js";
+import errorHandler from "../src/middlewares/errorHandler.js";
 
 export default function expressConfig(app) {
   const secret = "Cookie parser secret";
@@ -8,12 +9,15 @@ export default function expressConfig(app) {
   app.use(express.json());
   app.use(cookieParser(secret));
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static("static", {
-    setHeaders: (res, path) => {
-      if (path.endsWith('.css')) {
-        res.set('Content-Type', 'text/css');
-      }
-    }
-  }));
+  app.use(
+    express.static("static", {
+      setHeaders: (res, path) => {
+        if (path.endsWith(".css")) {
+          res.set("Content-Type", "text/css");
+        }
+      },
+    })
+  );
   app.use(auth());
+  app.use(errorHandler());
 }
--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,11 @@
 import { homeController } from "../src/controllers/home.js";
 import userRouter from "../src/controllers/user.js";
 import contactRouter from "../src/controllers/contact.js";
+import postRouter from "../src/controllers/post.js";
 
 export default function routes(app) {
   app.get("/", homeController);
   app.use(contactRouter);
   app.use(userRouter);
+  app.use(postRouter);
 }
