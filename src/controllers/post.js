cfffc04ce1bb2f2f62b87565a3977e6b963b8b94
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-02-02 14:24:48 +0200

    Added  post action for creating Posts and console.log the req.body to see its content

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import { isAuthenticated } from "../middlewares/guards.js";
+import { postValidator } from "../express-validator/post.js";
+import { validationResult } from "express-validator";
 
 const postRouter = Router();
 
 postRouter.get("/create", isAuthenticated(), (req, res) => {
   res.render("create");
 });
+
+postRouter.post(
+  "/create",
+  isAuthenticated(),
+  postValidator,
+  async (req, res) => {
+    //TODO: Implement post creation
+    console.log(req.body);
+  }
+);
 
 export default postRouter;
