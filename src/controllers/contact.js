a3701e435abd115f502c07c2df4332d2c2f0a054
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-03-02 13:48:41 +0200

    Added CSRF protection

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -3,25 +3,45 @@ import { contactValidator } from "../express-validator/contact.js";
 import { validationResult } from "express-validator";
 import sendEmail from "../services/contact.js";
 
+import { csrfSync } from "csrf-sync";
+
+const { generateToken } = csrfSync();
+
+const { csrfSynchronisedProtection } = csrfSync({
+  getTokenFromRequest: (req) => {
+    console.log(req.body._csrf);
+    return req.body["_csrf"];
+  }, // Used to retrieve the token submitted by the user in a form
+});
+
 const contactRouter = Router();
 
 contactRouter.get("/contact", (req, res) => {
-  res.render("contact");
+  const csrfToken = generateToken(req, true);
+  res.render("contact", { csrfToken });
 });
 
-contactRouter.post("/contact", contactValidator, async (req, res, next) => {
-  const errors = validationResult(req);
+contactRouter.post(
+  "/contact",
+  csrfSynchronisedProtection,
+  contactValidator,
+  async (req, res, next) => {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.render("contact", { errors: errors.array(), data: req.body });
-  }
+    if (!errors.isEmpty()) {
+      return res.render("contact", { errors: errors.array(), data: req.body });
+    }
 
-  try {
-    await sendEmail(req.body);
-    res.render("contact", { success: true, msg: "Email sent successfully !" });
-  } catch (error) {
-    next(error);
+    try {
+      await sendEmail(req.body);
+      res.render("contact", {
+        success: true,
+        msg: "Email sent successfully !",
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 export default contactRouter;
