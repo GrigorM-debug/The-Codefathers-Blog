fdcf480f6fb9d8fb5b69f6e97898243a86154193
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-03-09 11:29:04 +0200

    Updated the logic in getMessagesByRoomId method

--- src/services/message.js
+++ src/services/message.js
@@ -1,11 +1,13 @@
 import Message from "../models/Message.js";
 
-export async function createMessage(roomId, senderId, username, text) {
-  const message = new Message({ roomId, senderId, username, text });
+export async function createMessage(roomId, senderId, text) {
+  const message = new Message({ room: roomId, sender: senderId, text: text });
   return await message.save();
 }
 
 export async function getMessagesByRoomId(roomId) {
-  const message = await Message.find({ roomId });
+  const message = await Message.find({ room: roomId })
+    .populate("sender")
+    .lean();
   return message;
 }
