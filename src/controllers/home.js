8f9763f69fa8672146e56139bc109af0e4410447
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-05-12 19:23:24 +0300

    Implemneted AI Chat Bot Support using Chatbase ai agent

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,9 @@
+import "dotenv/config";
+
 export function homeController(req, res) {
-  res.render("home");
+  const CHATBASE_SRC = process.env.CHATBASE_SRC;
+  const CHATBASE_ID = process.env.CHATBASE_ID;
+  const CHATBASE_DOMAIN = process.env.CHATBASE_DOMAIN;
+
+  res.render("home", { CHATBASE_SRC, CHATBASE_ID, CHATBASE_DOMAIN });
 }
